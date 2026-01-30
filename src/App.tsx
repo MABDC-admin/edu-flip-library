41967441dcc65cf5089e82a59097e644dce62e97
MABDC-admin <admin@mabdc.ae>
2026-01-30 18:45:44 +0400

    feat(routing): remove landing page, set default to auth, hide admin link

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
-import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Bookshelf from "./pages/Bookshelf";
 import FlipbookReader from "./pages/FlipbookReader";
@@ -23,8 +22,7 @@ const App = () => (
           <Toaster />
           <Sonner />
           <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/auth" element={<Auth />} />
+            <Route path="/" element={<Auth />} />
             <Route path="/bookshelf" element={<Bookshelf />} />
             <Route path="/read/:bookId" element={<FlipbookReader />} />
             <Route path="/admin" element={<AdminDashboard />} />
