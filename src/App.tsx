4b1f2397ee00e81f444cbf4ae66dcc03c89c5dc8
MABDC-admin <admin@mabdc.ae>
2026-02-01 12:42:25 +0400

    Remove landing page, set Bookshelf as default route

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
-import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Bookshelf from "./pages/Bookshelf";
 import FlipbookReader from "./pages/FlipbookReader";
@@ -24,7 +23,7 @@ const App = () => (
           <Toaster />
           <Sonner />
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Bookshelf />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/bookshelf" element={<Bookshelf />} />
             <Route path="/read/:bookId" element={<FlipbookReader />} />
